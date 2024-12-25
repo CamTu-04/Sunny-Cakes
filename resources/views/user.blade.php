c43e772d6307369dc2c40512b8ed9f011cf70918
CamTu-04 <camtu041124@gmail.com>
2024-12-25 11:36:50 +0700

    Laravel

---
+++ resources/views/user.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    User is loaded
+    <h1>Hello Hoàng Cẩm Tú !</h1>
+</body>
+</html>
